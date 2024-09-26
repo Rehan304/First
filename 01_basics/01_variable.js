6d31b5bfc2f393e30eebb2c02e339d678950a37b
Rehan304 <rehanniaz556@gmail.com>
2024-09-26 17:19:46 +0000

    2nd

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,17 @@
+const accountId = 1452
+let accountEmail = "hi@gmail.com"
+let accountPassword = "23232"
+let accountcity = "232323"
+let accountState 
+//I'm still Trying
+accountEmail = "r2323@gmail.com"
+accountPassword = "A2323232323e"
+accountCity = "Bsdsp"
+
+console.log(accountId);
+/* 
+Never use a var 
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
