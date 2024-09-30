2ba798241c451d25831bfaec6483b26025ad98ce
Rehan304 <rehanniaz556@gmail.com>
2024-09-30 17:41:19 +0000

    Data types

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,10 @@
+"use strict"; //treat all js code as newer version
+//alert( 3 + 3) // using in nodejs not in a browser
+let Name = "Rehan"
+// Variable = String//
+let age = 21
+// Data type value variable = number value
+let isLoggedIn = true
+// These are bolean type
+console.log(typeof "Rehan");
+console.log(typeof age )
---
+++ 01_basics/Note.js
@@ -0,0 +1,20 @@
+/* Prefer Not to use 
+var due to some issues*/
+"use strict"; // treat all js code as newer version//
+// COde should be easily readable like as follows//
+console.log("Rehan");
+console.log("Password");
+// I wrote them is seperate lines//
+/* Original documentation tc39.es 
+we can also use mdn*/
+//Data entries variable includes
+// Numbers  => 2 power of 53 something like this
+//bigint used for storing large values like in fb, reddit ets
+//string => ""
+//boolean => true/false only
+//null => standalone value having no value different than undefined
+//undefined => means there is currently no value
+//symbol => mostly used for uniqueness
+//object 
+//console.log(typeof null) => object
+//console.log(typeof undefined) =>undefined
